a3a5a62d8eecbd1a76df5851b14c56617e7764ee
Zoff <zoff99@users.noreply.github.com>
2017-08-16 18:20:42 +0200

    add files

---
+++ java_ref_client/app/src/main/java/com/zoffcc/applications/trifa/TrifaToxService.java
@@ -0,0 +1,298 @@
+/**
+ * [TRIfA], Java part of Tox Reference Implementation for Android
+ * Copyright (C) 2017 Zoff <zoff@zoff.cc>
+ * <p>
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ * <p>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA  02110-1301, USA.
+ */
+
+package com.zoffcc.applications.trifa;
+
+import static com.zoffcc.applications.trifa.MainActivity.get_my_toxid;
+import static com.zoffcc.applications.trifa.MainActivity.tox_self_get_name;
+import static com.zoffcc.applications.trifa.MainActivity.tox_self_get_name_size;
+import static com.zoffcc.applications.trifa.MainActivity.tox_self_get_status_message;
+import static com.zoffcc.applications.trifa.MainActivity.tox_self_get_status_message_size;
+import static com.zoffcc.applications.trifa.MainActivity.tox_self_set_name;
+import static com.zoffcc.applications.trifa.MainActivity.tox_self_set_status_message;
+import static com.zoffcc.applications.trifa.MainActivity.tox_friend_add;
+import static com.zoffcc.applications.trifa.TRIFAGlobals.global_my_name;
+import static com.zoffcc.applications.trifa.TRIFAGlobals.global_my_status_message;
+import static com.zoffcc.applications.trifa.TRIFAGlobals.global_my_toxid;
+import static com.zoffcc.applications.trifa.TRIFAGlobals.bootstrapping;
+import static com.zoffcc.applications.trifa.TRIFAGlobals.ECHOBOT_TOXID;
+import static com.zoffcc.applications.trifa.TRIFAGlobals.GROUPBOT_TOXID;
+
+// import static com.zoffcc.applications.trifa.TRIFAGlobals.*;
+// import static com.zoffcc.applications.trifa.MainActivity.*;
+
+
+
+public class TrifaToxService
+{
+    static int ONGOING_NOTIFICATION_ID = 1030;
+    static final String TAG = "trifa.ToxService";
+    static Thread ToxServiceThread = null;
+    static boolean stop_me = false;
+    static boolean is_tox_started = false;
+    static boolean global_toxid_text_set = false;
+    static boolean TOX_SERVICE_STARTED = false;
+
+    static class Log
+    {
+        public static void i(String tag, String message)
+        {
+            System.out.println("" + tag + ":" + message + "\n");
+        }
+    }
+
+    void tox_thread_start_fg()
+    {
+        Log.i(TAG, "tox_thread_start_fg");
+
+        ToxServiceThread = new Thread()
+        {
+            @Override
+            public void run()
+            {
+
+                // ------ correct startup order ------
+                boolean old_is_tox_started = is_tox_started;
+                Log.i(TAG, "is_tox_started:==============================");
+                Log.i(TAG, "is_tox_started=" + is_tox_started);
+                Log.i(TAG, "is_tox_started:==============================");
+
+                is_tox_started = true;
+
+                if (!old_is_tox_started)
+                {
+                    MainActivity.init_tox_callbacks();
+                    MainActivity.update_savedata_file();
+                }
+                // ------ correct startup order ------
+
+                // TODO --------
+                String my_tox_id_local = get_my_toxid();
+                global_my_toxid = my_tox_id_local;
+                if (tox_self_get_name_size() > 0)
+                {
+                    global_my_name = tox_self_get_name().substring(0, (int) tox_self_get_name_size());
+                    Log.i(TAG, "AAA:003:" + global_my_name + " size=" + tox_self_get_name_size());
+                }
+                else
+                {
+                    tox_self_set_name("TRIfA " + my_tox_id_local.substring(my_tox_id_local.length() - 5, my_tox_id_local.length()));
+                    global_my_name = ("TRIfA " + my_tox_id_local.substring(my_tox_id_local.length() - 5, my_tox_id_local.length()));
+                    Log.i(TAG, "AAA:005");
+                }
+
+                if (tox_self_get_status_message_size() > 0)
+                {
+                    global_my_status_message = tox_self_get_status_message().substring(0, (int) tox_self_get_status_message_size());
+                    Log.i(TAG, "AAA:008:" + global_my_status_message + " size=" + tox_self_get_status_message_size());
+                }
+                else
+                {
+                    tox_self_set_status_message("this is TRIfA");
+                    global_my_status_message = "this is TRIfA";
+                    Log.i(TAG, "AAA:010");
+                }
+                Log.i(TAG, "AAA:011");
+
+                MainActivity.update_savedata_file();
+
+                MainActivity.friends = MainActivity.tox_self_get_friend_list();
+                Log.i(TAG, "loading_friend:number_of_friends=" + MainActivity.friends.length);
+
+                int fc = 0;
+                boolean exists_in_db = false;
+
+                for (fc = 0; fc < MainActivity.friends.length; fc++)
+                {
+
+                }
+
+
+                // --------------- bootstrap ---------------
+                // --------------- bootstrap ---------------
+                // --------------- bootstrap ---------------
+                if (!old_is_tox_started)
+                {
+                    bootstrapping = true;
+                    Log.i(TAG, "bootrapping:set to true");
+
+                    // ----- UDP ------
+                    Log.i(TAG, "bootstrap_single:res=" + MainActivity.bootstrap_single_wrapper("178.62.250.138", 33445, "788236D34978D1D5BD822F0A5BEBD2C53C64CC31CD3149350EE27D4D9A2F9B6B"));
+                    Log.i(TAG, "bootstrap_single:res=" + MainActivity.bootstrap_single_wrapper("nodes.tox.chat", 33445, "6FC41E2BD381D37E9748FC0E0328CE086AF9598BECC8FEB7DDF2E440475F300E"));
+                    Log.i(TAG, "bootstrap_single:res=" + MainActivity.bootstrap_single_wrapper("130.133.110.14", 33445, "461FA3776EF0FA655F1A05477DF1B3B614F7D6B124F7DB1DD4FE3C08B03B640F"));
+                    Log.i(TAG, "bootstrap_single:res=" + MainActivity.bootstrap_single_wrapper("tox.zodiaclabs.org", 33445, "A09162D68618E742FFBCA1C2C70385E6679604B2D80EA6E84AD0996A1AC8A074"));
+                    Log.i(TAG, "bootstrap_single:res=" + MainActivity.bootstrap_single_wrapper("163.172.136.118", 33445, "2C289F9F37C20D09DA83565588BF496FAB3764853FA38141817A72E3F18ACA0B"));
+                    Log.i(TAG, "bootstrap_single:res=" + MainActivity.bootstrap_single_wrapper("217.182.143.254", 443, "7AED21F94D82B05774F697B209628CD5A9AD17E0C073D9329076A4C28ED28147"));
+                    Log.i(TAG, "bootstrap_single:res=" + MainActivity.bootstrap_single_wrapper("185.14.30.213", 443, "2555763C8C460495B14157D234DD56B86300A2395554BCAE4621AC345B8C1B1B"));
+                    Log.i(TAG, "bootstrap_single:res=" + MainActivity.bootstrap_single_wrapper("136.243.141.187", 443, "6EE1FADE9F55CC7938234CC07C864081FC606D8FE7B751EDA217F268F1078A39"));
+                    Log.i(TAG, "bootstrap_single:res=" + MainActivity.bootstrap_single_wrapper("128.199.199.197", 33445, "B05C8869DBB4EDDD308F43C1A974A20A725A36EACCA123862FDE9945BF9D3E09"));
+                    Log.i(TAG, "bootstrap_single:res=" + MainActivity.bootstrap_single_wrapper("biribiri.org", 33445, "F404ABAA1C99A9D37D61AB54898F56793E1DEF8BD46B1038B9D822E8460FAB67"));
+                    // ----- UDP ------
+                    //
+                    // ----- TCP ------
+                    Log.i(TAG, "add_tcp_relay_single:res=" + MainActivity.add_tcp_relay_single_wrapper("178.62.250.138", 33445, "788236D34978D1D5BD822F0A5BEBD2C53C64CC31CD3149350EE27D4D9A2F9B6B"));
+                    Log.i(TAG, "add_tcp_relay_single:res=" + MainActivity.add_tcp_relay_single_wrapper("nodes.tox.chat", 33445, "6FC41E2BD381D37E9748FC0E0328CE086AF9598BECC8FEB7DDF2E440475F300E"));
+                    Log.i(TAG, "add_tcp_relay_single:res=" + MainActivity.add_tcp_relay_single_wrapper("130.133.110.14", 33445, "461FA3776EF0FA655F1A05477DF1B3B614F7D6B124F7DB1DD4FE3C08B03B640F"));
+                    Log.i(TAG, "add_tcp_relay_single:res=" + MainActivity.add_tcp_relay_single_wrapper("tox.zodiaclabs.org", 33445, "A09162D68618E742FFBCA1C2C70385E6679604B2D80EA6E84AD0996A1AC8A074"));
+                    Log.i(TAG, "add_tcp_relay_single:res=" + MainActivity.add_tcp_relay_single_wrapper("163.172.136.118", 33445, "2C289F9F37C20D09DA83565588BF496FAB3764853FA38141817A72E3F18ACA0B"));
+                    Log.i(TAG, "add_tcp_relay_single:res=" + MainActivity.add_tcp_relay_single_wrapper("217.182.143.254", 443, "7AED21F94D82B05774F697B209628CD5A9AD17E0C073D9329076A4C28ED28147"));
+                    Log.i(TAG, "add_tcp_relay_single:res=" + MainActivity.add_tcp_relay_single_wrapper("185.14.30.213", 443, "2555763C8C460495B14157D234DD56B86300A2395554BCAE4621AC345B8C1B1B"));
+                    Log.i(TAG, "add_tcp_relay_single:res=" + MainActivity.add_tcp_relay_single_wrapper("136.243.141.187", 443, "6EE1FADE9F55CC7938234CC07C864081FC606D8FE7B751EDA217F268F1078A39"));
+                    Log.i(TAG, "add_tcp_relay_single:res=" + MainActivity.add_tcp_relay_single_wrapper("128.199.199.197", 33445, "B05C8869DBB4EDDD308F43C1A974A20A725A36EACCA123862FDE9945BF9D3E09"));
+                    Log.i(TAG, "add_tcp_relay_single:res=" + MainActivity.add_tcp_relay_single_wrapper("biribiri.org", 33445, "F404ABAA1C99A9D37D61AB54898F56793E1DEF8BD46B1038B9D822E8460FAB67"));
+                    // ----- TCP ------
+                }
+
+                // --------------- bootstrap ---------------
+                // --------------- bootstrap ---------------
+                // --------------- bootstrap ---------------
+
+                long tox_iteration_interval_ms = MainActivity.tox_iteration_interval();
+                Log.i(TAG, "tox_iteration_interval_ms=" + tox_iteration_interval_ms);
+
+                MainActivity.tox_iterate();
+
+				// --- add echobot and groupbot ---
+				long friendnum;
+				friendnum = tox_friend_add(ECHOBOT_TOXID, "please add me");
+				Log.i(TAG, "tox_friend_add:add friend #:" + friendnum);
+				friendnum = tox_friend_add(GROUPBOT_TOXID, "please add me");
+				Log.i(TAG, "tox_friend_add:add friend #:" + friendnum);
+				// --- add echobot and groupbot ---
+
+
+                // ------- MAIN TOX LOOP ---------------------------------------------------------------
+                // ------- MAIN TOX LOOP ---------------------------------------------------------------
+                // ------- MAIN TOX LOOP ---------------------------------------------------------------
+                // ------- MAIN TOX LOOP ---------------------------------------------------------------
+                // ------- MAIN TOX LOOP ---------------------------------------------------------------
+                while (!stop_me)
+                {
+                    try
+                    {
+                        if (tox_iteration_interval_ms < 3)
+                        {
+                            Log.i(TAG, "tox_iterate:(tox_iteration_interval_ms < 2ms!!):" + tox_iteration_interval_ms + "ms");
+                            Thread.sleep(3);
+                        }
+                        else
+                        {
+                            // Log.i(TAG, "(tox_iteration_interval_ms):" + tox_iteration_interval_ms + "ms");
+                            Thread.sleep(tox_iteration_interval_ms);
+                        }
+                    }
+                    catch (InterruptedException e)
+                    {
+                        e.printStackTrace();
+                    }
+                    catch (Exception e)
+                    {
+                        e.printStackTrace();
+                    }
+                    // Log.i(TAG, "tox_iterate:--START--");
+                    long s_time = System.currentTimeMillis();
+                    MainActivity.tox_iterate();
+                    if (s_time + 4000 < System.currentTimeMillis())
+                    {
+                        tox_iteration_interval_ms = MainActivity.tox_iteration_interval();
+                        Log.i(TAG, "tox_iterate:--END--:took" + (long) (((float) (s_time - System.currentTimeMillis()) / 1000f)) + "s, new inerval=" + tox_iteration_interval_ms + "ms");
+                    }
+                }
+                // ------- MAIN TOX LOOP ---------------------------------------------------------------
+                // ------- MAIN TOX LOOP ---------------------------------------------------------------
+                // ------- MAIN TOX LOOP ---------------------------------------------------------------
+                // ------- MAIN TOX LOOP ---------------------------------------------------------------
+                // ------- MAIN TOX LOOP ---------------------------------------------------------------
+
+
+                try
+                {
+                    Thread.sleep(100); // wait a bit, for "something" to finish up in the native code
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+
+                try
+                {
+                    MainActivity.tox_kill();
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+
+                try
+                {
+                    Thread.sleep(100); // wait a bit, for "something" to finish up in the native code
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+
+            }
+        };
+
+        ToxServiceThread.start();
+    }
+
+    // ------------------------------
+
+
+    // --------------- JNI ---------------
+    // --------------- JNI ---------------
+    // --------------- JNI ---------------
+    static void logger(int level, String text)
+    {
+        Log.i(TAG, text);
+    }
+
+    static String safe_string(byte[] in)
+    {
+        // Log.i(TAG, "safe_string:in=" + in);
+        String out = "";
+
+        try
+        {
+            out = new String(in, "UTF-8");  // Best way to decode using "UTF-8"
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            Log.i(TAG, "safe_string:EE:" + e.getMessage());
+            try
+            {
+                out = new String(in);
+            }
+            catch (Exception e2)
+            {
+                e2.printStackTrace();
+                Log.i(TAG, "safe_string:EE2:" + e2.getMessage());
+            }
+        }
+
+        // Log.i(TAG, "safe_string:out=" + out);
+        return out;
+    }
+    // --------------- JNI ---------------
+    // --------------- JNI ---------------
+    // --------------- JNI ---------------
+}
