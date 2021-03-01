d2efd67746cf3d6eec46d6f231b2977795b3e4ab
zoff99 <zoff99@users.noreply.github.com>
2021-03-01 19:03:18 +0100

    optional dpi factor

--- trifa_desktop/app/src/main/java/do_run.sh
+++ trifa_desktop/app/src/main/java/do_run.sh
@@ -1,6 +1,12 @@
 #! /bin/bash
 
-java -Dsun.java2d.uiScale=3 \
+if [ "$1""x" != "x" ]; then
+    scale='-Dsun.java2d.uiScale='"$1"
+else
+    scale=""
+fi
+
+java $scale \
 -Djava.library.path="." \
 -classpath ".:sqlite-jdbc-3.32.3.2.jar:webcam-capture-0.3.12.jar:bridj-0.7.0.jar:slf4j-api-1.7.2.jar" \
 com.zoffcc.applications.trifa.MainActivity 2>&1 | tee trifa.log
