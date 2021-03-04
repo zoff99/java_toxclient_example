300af9e265dec7185a1ea3c749cfbd38b1f9284f
zoff99 <zoff99@users.noreply.github.com>
2021-03-04 16:36:31 +0100

    add lookandfeel jar

--- trifa_desktop/app/src/main/java/do_run.bat
+++ trifa_desktop/app/src/main/java/do_run.bat
@@ -1,1 +1,1 @@
-java.exe -Djava.library.path="." -classpath ".;sqlite-jdbc-3.32.3.2.jar;webcam-capture-0.3.12.jar;bridj-0.7.0.jar;slf4j-api-1.7.2.jar" com.zoffcc.applications.trifa.MainActivity
+java.exe -Djava.library.path="." -classpath ".;sqlite-jdbc-3.32.3.2.jar;webcam-capture-0.3.12.jar;bridj-0.7.0.jar;slf4j-api-1.7.2.jar;flatlaf-1.0.jar" com.zoffcc.applications.trifa.MainActivity
