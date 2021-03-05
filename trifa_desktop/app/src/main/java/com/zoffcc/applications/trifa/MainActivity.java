22db44383877f4b8152a978ee54853d42a191761
zoff99 <zoff99@users.noreply.github.com>
2021-03-05 18:24:24 +0100

    1.0.10

--- trifa_desktop/app/src/main/java/com/zoffcc/applications/trifa/MainActivity.java
+++ trifa_desktop/app/src/main/java/com/zoffcc/applications/trifa/MainActivity.java
@@ -117,7 +117,7 @@ import static javax.swing.JOptionPane.YES_OPTION;
 public class MainActivity extends JFrame
 {
     private static final String TAG = "trifa.MainActivity";
-    static final String Version = "1.0.9";
+    static final String Version = "1.0.10";
     // --------- global config ---------
     // --------- global config ---------
     // --------- global config ---------
