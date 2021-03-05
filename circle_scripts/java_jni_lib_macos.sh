6e6ea6f36470c4260e75cac04f556dec1e315b8a
zoff99 <zoff99@users.noreply.github.com>
2021-03-05 17:03:11 +0100

    run GUI app, take screenshots

--- circle_scripts/java_jni_lib_macos.sh
+++ circle_scripts/java_jni_lib_macos.sh
@@ -116,24 +116,35 @@ $JAVA_HOME/bin/java -version
 $JAVA_HOME/bin/javac -version
 
 # --------- compile java example ---------
-cd /Users/runner/work/java_toxclient_example/java_toxclient_example/java_ref_client/app/src/main/java/
-$JAVA_HOME/bin/javac com/zoffcc/applications/trifa/ToxVars.java
-$JAVA_HOME/bin/javac com/zoffcc/applications/trifa/TRIFAGlobals.java
-$JAVA_HOME/bin/javac com/zoffcc/applications/trifa/MainActivity.java
-$JAVA_HOME/bin/javac com/zoffcc/applications/trifa/TrifaToxService.java
+#cd /Users/runner/work/java_toxclient_example/java_toxclient_example/java_ref_client/app/src/main/java/
+#$JAVA_HOME/bin/javac com/zoffcc/applications/trifa/ToxVars.java
+#$JAVA_HOME/bin/javac com/zoffcc/applications/trifa/TRIFAGlobals.java
+#$JAVA_HOME/bin/javac com/zoffcc/applications/trifa/MainActivity.java
+#$JAVA_HOME/bin/javac com/zoffcc/applications/trifa/TrifaToxService.java
 # --------- package java example ---------
-cd /Users/runner/work/java_toxclient_example/java_toxclient_example/java_ref_client/app/src/main/java/
+#cd /Users/runner/work/java_toxclient_example/java_toxclient_example/java_ref_client/app/src/main/java/
+
+
+# --------- compile java example ---------
+cd /Users/runner/work/java_toxclient_example/java_toxclient_example/trifa_desktop/app/src/main/java/
+$JAVA_HOME/bin/javac \
+-classpath ".:sqlite-jdbc-3.32.3.2.jar:webcam-capture-0.3.12.jar:bridj-0.7.0.jar:slf4j-api-1.7.2.jar:flatlaf-1.0.jar" \
+./com/zoffcc/applications/trifa/TRIFAGlobals.java ./com/zoffcc/applications/trifa/Table.java ./com/zoffcc/applications/trifa/OnConflict.java ./com/zoffcc/applications/trifa/CombinedFriendsAndConferences.java ./com/zoffcc/applications/trifa/OrmaDatabase.java ./com/zoffcc/applications/trifa/HelperGeneric.java ./com/zoffcc/applications/trifa/ConferencePeerCacheDB.java ./com/zoffcc/applications/trifa/Index.java ./com/zoffcc/applications/trifa/AudioFrame.java ./com/zoffcc/applications/trifa/FullscreenToggleAction.java ./com/zoffcc/applications/trifa/Nullable.java ./com/zoffcc/applications/trifa/Column.java ./com/zoffcc/applications/trifa/FriendList.java ./com/zoffcc/applications/trifa/SingleComponentAspectRatioKeeperLayout.java ./com/zoffcc/applications/trifa/HelperFriend.java ./com/zoffcc/applications/trifa/JPictureBox.java ./com/zoffcc/applications/trifa/MainActivity.java ./com/zoffcc/applications/trifa/TrifaToxService.java ./com/zoffcc/applications/trifa/MessageListFragmentJ.java ./com/zoffcc/applications/trifa/AudioSelectOutBox.java ./com/zoffcc/applications/trifa/ByteBufferCompat.java ./com/zoffcc/applications/trifa/AudioBar.java ./com/zoffcc/applications/trifa/HelperMessage.java ./com/zoffcc/applications/trifa/VideoOutFrame.java ./com/zoffcc/applications/trifa/ConferenceDB.java ./com/zoffcc/applications/trifa/FriendListFragmentJ.java ./com/zoffcc/applications/trifa/Callstate.java ./com/zoffcc/applications/trifa/Log.java ./com/zoffcc/applications/trifa/PrimaryKey.java ./com/zoffcc/applications/trifa/RelayListDB.java ./com/zoffcc/applications/trifa/Message.java ./com/zoffcc/applications/trifa/ToxVars.java ./com/zoffcc/applications/trifa/VideoInFrame.java ./com/zoffcc/applications/trifa/HelperConference.java ./com/zoffcc/applications/trifa/AudioSelectInBox.java ./com/zoffcc/applications/trifa/TRIFADatabaseGlobalsNew.java ./com/zoffcc/applications/trifa/BootstrapNodeEntryDB.java ./com/zoffcc/applications/trifa/HelperRelay.java
+# --------- package java example ---------
+
 mkdir -p /Users/runner/work/artefacts/
-tar -cvf /Users/runner/work/artefacts/install_macos.tar com *.sh *.jnilib
+tar -cvf /Users/runner/work/artefacts/install_macos.tar com assets i18n *.sh *.jnilib *.jar
 # --------- run test java application ---------
-$JAVA_HOME/bin/java -Djava.library.path="." com.zoffcc.applications.trifa.MainActivity > trifa.log 2>&1 &
+$JAVA_HOME/bin/java  -Djava.library.path="." \
+-classpath ".:sqlite-jdbc-3.32.3.2.jar:webcam-capture-0.3.12.jar:bridj-0.7.0.jar:slf4j-api-1.7.2.jar:flatlaf-1.0.jar" \
+com.zoffcc.applications.trifa.MainActivity > trifa.log 2>&1 &
 # --------- run test java application ---------
-sleep 10
+sleep 20
 cat ./trifa.log|head -20
 echo
 echo
-cat ./trifa.log | grep 'MyToxID:' | cut -d':' -f 3
+cat ./trifa.log | grep 'MyToxID:' | cut -d':' -f 8
 echo
 echo
-sleep 40
+screencapture -T 1 -x -t png /Users/runner/screen101.png
 
