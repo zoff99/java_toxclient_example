d67e8250b6b87f4dccf4916dab33c438ef2d30b8
zoff99 <zoff99@users.noreply.github.com>
2021-03-09 15:33:12 +0100

    run from other directory

--- trifa_desktop/app/src/main/java/do_run.sh
+++ trifa_desktop/app/src/main/java/do_run.sh
@@ -1,5 +1,13 @@
 #! /bin/bash
 
+_HOME2_=$(dirname $0)
+export _HOME2_
+_HOME_=$(cd $_HOME2_;pwd)
+export _HOME_
+
+echo $_HOME_
+cd $_HOME_
+
 if [ "$1""x" != "x" ]; then
     scale='-Dsun.java2d.uiScale='"$1"
 else
