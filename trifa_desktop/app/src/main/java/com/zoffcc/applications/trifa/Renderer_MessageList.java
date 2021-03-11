3511365d2c6f09cd2b86fc1a1119cbeb58a49ba7
zoff99 <zoff99@users.noreply.github.com>
2021-03-11 19:29:49 +0100

    replace unicode char with code, windows compile script

--- trifa_desktop/app/src/main/java/com/zoffcc/applications/trifa/Renderer_MessageList.java
+++ trifa_desktop/app/src/main/java/com/zoffcc/applications/trifa/Renderer_MessageList.java
@@ -102,7 +102,7 @@ public class Renderer_MessageList extends JPanel implements ListCellRenderer
         final String unicode_INCOMING_ENVELOPE = "\uD83D\uDCE8";
         final String unicode_Mobile_Phone_With_Arrow = "\uD83D\uDCF2";
         final String unicode_MEMO = "\uD83D\uDCDD";
-        final String unicode_ARROW_LEFT = "←";
+        final String unicode_ARROW_LEFT = "\u2190";
 
         String is_read = "     ";
 
