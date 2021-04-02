88a3d558d32b403948775a5546ecbcdc26c0858c
zoff99 <zoff99@users.noreply.github.com>
2021-04-02 14:57:55 +0200

    package release zip

--- trifa_desktop/app/src/main/java/__make_release.sh
+++ trifa_desktop/app/src/main/java/__make_release.sh
@@ -20,6 +20,8 @@ cp -a \
   io \
   i18n \
   \
+  json-20210307.jar \
+  emoji-java-5.1.1.jar \
   bridj-0.7.0.jar \
   slf4j-api-1.7.2.jar \
   webcam-capture-0.3.12.jar \
@@ -27,7 +29,7 @@ cp -a \
   flatlaf-1.0.jar \
   \
   jni-c-toxcore.dll \
-  libjni-c-toxcore.jnilib \
+  libjni-c-toxcore.jnilib* \
   libjni-c-toxcore.so \
   \
   do_compile.bat \
