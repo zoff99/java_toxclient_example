2d195a3a7ba0689d51eb35079073a47f4a0c87aa
zoff99 <zoff99@users.noreply.github.com>
2021-05-13 10:46:53 +0200

    1.0.18

--- trifa_desktop/app/src/main/java/com/zoffcc/applications/trifa/MainActivity.java
+++ trifa_desktop/app/src/main/java/com/zoffcc/applications/trifa/MainActivity.java
@@ -181,7 +181,7 @@ import static javax.swing.JOptionPane.YES_OPTION;
 public class MainActivity extends JFrame
 {
     private static final String TAG = "trifa.MainActivity";
-    static final String Version = "1.0.17";
+    static final String Version = "1.0.18";
     // --------- global config ---------
     // --------- global config ---------
     final static boolean CTOXCORE_NATIVE_LOGGING = false; // set "false" for release builds
