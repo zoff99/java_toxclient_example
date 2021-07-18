e4123e4aeff10f87d426d17198a1ab5955180c81
zoff99 <zoff99@users.noreply.github.com>
2021-07-18 10:57:16 +0200

    update bootstrap nodes from nodes.tox.chat

--- trifa_desktop/app/src/main/java/com/zoffcc/applications/trifa/TrifaToxService.java
+++ trifa_desktop/app/src/main/java/com/zoffcc/applications/trifa/TrifaToxService.java
@@ -194,8 +194,6 @@ public class TrifaToxService
                     Log.i(TAG, "bootrapping:set to true");
 
                     // ----- UDP ------
-                    MainActivity.bootstrap_single_wrapper("85.172.30.117", 33445,
-                                                          "8E7D0B859922EF569298B4D261A8CCB5FEA14FB91ED412A7603A585A25698832");
                     MainActivity.bootstrap_single_wrapper("85.143.221.42", 33445,
                                                           "DA4E4ED4B697F2E9B000EEFE3A34B554ACD3F45F5C96EAEA2516DD7FF9AF7B43");
                     MainActivity.bootstrap_single_wrapper("tox.verdict.gg", 33445,
@@ -224,50 +222,44 @@ public class TrifaToxService
                                                           "3091C6BEB2A993F1C6300C16549FABA67098FF3D62C6D253828B531470B53D68");
                     MainActivity.bootstrap_single_wrapper("tox2.abilinski.com", 33445,
                                                           "7A6098B590BDC73F9723FC59F82B3F9085A64D1B213AAF8E610FD351930D052D");
-                    MainActivity.bootstrap_single_wrapper("floki.blog", 33445,
-                                                          "6C6AF2236F478F8305969CCFC7A7B67C6383558FF87716D38D55906E08E72667");
                     MainActivity.bootstrap_single_wrapper("46.101.197.175", 33445,
                                                           "CD133B521159541FB1D326DE9850F5E56A6C724B5B8E5EB5CD8D950408E95707");
                     MainActivity.bootstrap_single_wrapper("tox1.mf-net.eu", 33445,
                                                           "B3E5FA80DC8EBD1149AD2AB35ED8B85BD546DEDE261CA593234C619249419506");
                     MainActivity.bootstrap_single_wrapper("tox2.mf-net.eu", 33445,
                                                           "70EA214FDE161E7432530605213F18F7427DC773E276B3E317A07531F548545F");
-                    MainActivity.bootstrap_single_wrapper("46.146.229.184", 33445,
-                                                          "94750E94013586CCD989233A621747E2646F08F31102339452CADCF6DC2A760A");
                     MainActivity.bootstrap_single_wrapper("195.201.7.101", 33445,
                                                           "B84E865125B4EC4C368CD047C72BCE447644A2DC31EF75BD2CDA345BFD310107");
                     MainActivity.bootstrap_single_wrapper("168.138.203.178", 33445,
                                                           "6D04D8248E553F6F0BFDDB66FBFB03977E3EE54C432D416BC2444986EF02CC17");
-                    MainActivity.bootstrap_single_wrapper("5.19.249.240", 38296,
-                                                          "DA98A4C0CD7473A133E115FEA2EBDAEEA2EF4F79FD69325FC070DA4DE4BA3238");
                     MainActivity.bootstrap_single_wrapper("209.59.144.175", 33445,
                                                           "214B7FEA63227CAEC5BCBA87F7ABEEDB1A2FF6D18377DD86BF551B8E094D5F1E");
                     MainActivity.bootstrap_single_wrapper("188.225.9.167", 33445,
-                                                          "098AD1859B0F29894C49DBD108689432047F79CE57DD2BBDAD2E638C85521F2E");
+                                                          "1911341A83E02503AB1FD6561BD64AF3A9D6C3F12B5FBB656976B2E678644A67");
                     MainActivity.bootstrap_single_wrapper("122.116.39.151", 33445,
                                                           "5716530A10D362867C8E87EE1CD5362A233BAFBBA4CF47FA73B7CAD368BD5E6E");
                     MainActivity.bootstrap_single_wrapper("195.123.208.139", 33445,
                                                           "534A589BA7427C631773D13083570F529238211893640C99D1507300F055FE73");
                     MainActivity.bootstrap_single_wrapper("208.38.228.104", 33445,
                                                           "3634666A51CA5BE1579C031BD31B20059280EB7C05406ED466BD9DFA53373271");
-                    MainActivity.bootstrap_single_wrapper("lunarfire.spdns.org", 33445,
-                                                          "E61F5963268A6306CCFE7AF98716345235763529957BD5F45889484654EE052B");
                     MainActivity.bootstrap_single_wrapper("104.225.141.59", 43334,
                                                           "933BA20B2E258B4C0D475B6DECE90C7E827FE83EFA9655414E7841251B19A72C");
+                    MainActivity.bootstrap_single_wrapper("137.74.42.224", 33445,
+                                                          "A95177FA018066CF044E811178D26B844CBF7E1E76F140095B3A1807E081A204");
+                    MainActivity.bootstrap_single_wrapper("198.98.49.206", 33445,
+                                                          "28DB44A3CEEE69146469855DFFE5F54DA567F5D65E03EFB1D38BBAEFF2553255");
                     // ----- UDP ------
                     //
                     // ----- TCP ------
-                    MainActivity.add_tcp_relay_single_wrapper("85.172.30.117", 33445,
-                                                              "8E7D0B859922EF569298B4D261A8CCB5FEA14FB91ED412A7603A585A25698832");
                     MainActivity.add_tcp_relay_single_wrapper("85.143.221.42", 3389,
                                                               "DA4E4ED4B697F2E9B000EEFE3A34B554ACD3F45F5C96EAEA2516DD7FF9AF7B43");
-                    MainActivity.add_tcp_relay_single_wrapper("tox.verdict.gg", 3389,
+                    MainActivity.add_tcp_relay_single_wrapper("tox.verdict.gg", 33445,
                                                               "1C5293AEF2114717547B39DA8EA6F1E331E5E358B35F9B6B5F19317911C5F976");
-                    MainActivity.add_tcp_relay_single_wrapper("78.46.73.141", 3389,
+                    MainActivity.add_tcp_relay_single_wrapper("78.46.73.141", 33445,
                                                               "02807CF4F8BB8FB390CC3794BDF1E8449E9A8392C5D3F2200019DA9F1E812E46");
                     MainActivity.add_tcp_relay_single_wrapper("tox.initramfs.io", 3389,
                                                               "3F0A45A268367C1BEA652F258C85F4A66DA76BCAA667A49E770BCC4917AB6A25");
-                    MainActivity.add_tcp_relay_single_wrapper("144.217.167.73", 33445,
+                    MainActivity.add_tcp_relay_single_wrapper("144.217.167.73", 3389,
                                                               "7E5668E0EE09E19F320AD47902419331FFEE147BB3606769CFBE921A2A2FD34C");
                     MainActivity.add_tcp_relay_single_wrapper("tox.abilinski.com", 33445,
                                                               "10C00EB250C3233E343E2AEBA07115A5C28920E9C8D29492F6D00B29049EDC7E");
@@ -279,38 +271,38 @@ public class TrifaToxService
                                                               "BEF0CFB37AF874BD17B9A8F9FE64C75521DB95A37D33C5BDB00E9CF58659C04F");
                     MainActivity.add_tcp_relay_single_wrapper("tox.kurnevsky.net", 33445,
                                                               "82EF82BA33445A1F91A7DB27189ECFC0C013E06E3DA71F588ED692BED625EC23");
-                    MainActivity.add_tcp_relay_single_wrapper("81.169.136.229", 3389,
+                    MainActivity.add_tcp_relay_single_wrapper("81.169.136.229", 33445,
                                                               "E0DB78116AC6500398DDBA2AEEF3220BB116384CAB714C5D1FCD61EA2B69D75E");
-                    MainActivity.add_tcp_relay_single_wrapper("205.185.115.131", 53,
+                    MainActivity.add_tcp_relay_single_wrapper("205.185.115.131", 3389,
                                                               "3091C6BEB2A993F1C6300C16549FABA67098FF3D62C6D253828B531470B53D68");
                     MainActivity.add_tcp_relay_single_wrapper("tox2.abilinski.com", 33445,
                                                               "7A6098B590BDC73F9723FC59F82B3F9085A64D1B213AAF8E610FD351930D052D");
                     MainActivity.add_tcp_relay_single_wrapper("46.101.197.175", 33445,
                                                               "CD133B521159541FB1D326DE9850F5E56A6C724B5B8E5EB5CD8D950408E95707");
-                    MainActivity.add_tcp_relay_single_wrapper("tox1.mf-net.eu", 33445,
+                    MainActivity.add_tcp_relay_single_wrapper("tox1.mf-net.eu", 3389,
                                                               "B3E5FA80DC8EBD1149AD2AB35ED8B85BD546DEDE261CA593234C619249419506");
-                    MainActivity.add_tcp_relay_single_wrapper("tox2.mf-net.eu", 33445,
+                    MainActivity.add_tcp_relay_single_wrapper("tox2.mf-net.eu", 3389,
                                                               "70EA214FDE161E7432530605213F18F7427DC773E276B3E317A07531F548545F");
-                    MainActivity.add_tcp_relay_single_wrapper("46.146.229.184", 33445,
-                                                              "94750E94013586CCD989233A621747E2646F08F31102339452CADCF6DC2A760A");
                     MainActivity.add_tcp_relay_single_wrapper("195.201.7.101", 33445,
                                                               "B84E865125B4EC4C368CD047C72BCE447644A2DC31EF75BD2CDA345BFD310107");
                     MainActivity.add_tcp_relay_single_wrapper("168.138.203.178", 33445,
                                                               "6D04D8248E553F6F0BFDDB66FBFB03977E3EE54C432D416BC2444986EF02CC17");
-                    MainActivity.add_tcp_relay_single_wrapper("5.19.249.240", 3389,
-                                                              "DA98A4C0CD7473A133E115FEA2EBDAEEA2EF4F79FD69325FC070DA4DE4BA3238");
                     MainActivity.add_tcp_relay_single_wrapper("209.59.144.175", 33445,
                                                               "214B7FEA63227CAEC5BCBA87F7ABEEDB1A2FF6D18377DD86BF551B8E094D5F1E");
                     MainActivity.add_tcp_relay_single_wrapper("188.225.9.167", 33445,
-                                                              "098AD1859B0F29894C49DBD108689432047F79CE57DD2BBDAD2E638C85521F2E");
-                    MainActivity.add_tcp_relay_single_wrapper("122.116.39.151", 3389,
+                                                              "1911341A83E02503AB1FD6561BD64AF3A9D6C3F12B5FBB656976B2E678644A67");
+                    MainActivity.add_tcp_relay_single_wrapper("122.116.39.151", 33445,
                                                               "5716530A10D362867C8E87EE1CD5362A233BAFBBA4CF47FA73B7CAD368BD5E6E");
                     MainActivity.add_tcp_relay_single_wrapper("195.123.208.139", 3389,
                                                               "534A589BA7427C631773D13083570F529238211893640C99D1507300F055FE73");
                     MainActivity.add_tcp_relay_single_wrapper("208.38.228.104", 33445,
                                                               "3634666A51CA5BE1579C031BD31B20059280EB7C05406ED466BD9DFA53373271");
-                    MainActivity.add_tcp_relay_single_wrapper("lunarfire.spdns.org", 33445,
-                                                              "E61F5963268A6306CCFE7AF98716345235763529957BD5F45889484654EE052B");
+                    MainActivity.add_tcp_relay_single_wrapper("137.74.42.224", 33445,
+                                                              "A95177FA018066CF044E811178D26B844CBF7E1E76F140095B3A1807E081A204");
+                    MainActivity.add_tcp_relay_single_wrapper("198.98.49.206", 33445,
+                                                              "28DB44A3CEEE69146469855DFFE5F54DA567F5D65E03EFB1D38BBAEFF2553255");
+                    MainActivity.add_tcp_relay_single_wrapper("5.19.249.240", 3389,
+                                                              "DA98A4C0CD7473A133E115FEA2EBDAEEA2EF4F79FD69325FC070DA4DE4BA3238");
                     // ----- TCP ------
                 }
 
