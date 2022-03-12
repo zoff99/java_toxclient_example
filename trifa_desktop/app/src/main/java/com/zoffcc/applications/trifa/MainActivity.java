084828d5d013e780cbbd9d72b2e5f4adb8b2eca1
zoff99 <zoff99@users.noreply.github.com>
2022-03-12 11:10:26 +0100

    1.0.37

--- trifa_desktop/app/src/main/java/com/zoffcc/applications/trifa/MainActivity.java
+++ trifa_desktop/app/src/main/java/com/zoffcc/applications/trifa/MainActivity.java
@@ -214,7 +214,7 @@ import static javax.swing.JOptionPane.YES_OPTION;
 public class MainActivity extends JFrame implements WindowListener, WindowFocusListener, WindowStateListener
 {
     private static final String TAG = "trifa.MainActivity";
-    static final String Version = "1.0.36";
+    static final String Version = "1.0.37";
     // --------- global config ---------
     // --------- global config ---------
     final static boolean CTOXCORE_NATIVE_LOGGING = false; // set "false" for release builds
